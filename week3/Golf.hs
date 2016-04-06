e5d7eb854e9f6ffe809af85d50d8e6dd2e9e3d4e
Tom Wadeson <tom@tomwadeson.com>
2016-04-06 20:45:11 +0100

    Homework for week 3

--- week3/Golf.hs
+++ week3/Golf.hs
@@ -1,4 +1,3 @@
-{-# OPTIONS_GHC -Wall #-}
 module Golf where
 
 import Data.List (tails)
@@ -19,8 +18,14 @@ localMaxima = concatMap lm . filter (\x -> length x == 3) . map (take 3) . tails
       | otherwise      = []
 
 histogram :: [Integer] -> String
-histogram = show . map (\xs -> map (\x -> if x `elem` xs then '*' else ' ') [0..9]) . mkRows 0 . foldr (\x acc -> M.insertWith (+) x 1 acc) M.empty
+histogram = draw . plot . filter (not . null) . mkRows 1 . mkFrequencyMap
   where
+    mkFrequencyMap = foldr (\x acc -> M.insertWith (+) x 1 acc) M.empty
+
     mkRows n hist
-      | n <= 9    = (M.keys . M.filter (>= n) $ hist) : mkRows (n+1) hist 
+      | n <= 10   = (M.keys . M.filter (>= n) $ hist) : mkRows (n+1) hist
       | otherwise = []
+    
+    plot = map (\xs -> map (\x -> if x `elem` xs then '*' else ' ') [0..9])
+
+    draw xs = unlines . reverse $ ("0123456789" : xs)
