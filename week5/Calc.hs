961f31356e1b925fbf9dc2b9d6f0e0a0c19d7aa3
Tom Wadeson <tom@tomwadeson.com>
2016-04-12 06:32:44 +0100

    Solve exercise 2 of homework 5

--- week5/Calc.hs
+++ week5/Calc.hs
@@ -3,8 +3,12 @@
 module Calc where
 
 import ExprT
+import Parser
 
 eval :: ExprT -> Integer
 eval (Lit x)           = x
 eval (Add expr1 expr2) = (eval expr1) + (eval expr2)
 eval (Mul expr1 expr2) = (eval expr1) * (eval expr2)
+
+evalStr :: String -> Maybe Integer
+evalStr = fmap eval . parseExp Lit Add Mul
