3dbcba4d7fee880fe78128f19f3fae537f0407ae
Tom Wadeson <tom@tomwadeson.com>
2016-04-12 07:15:36 +0100

    Solve exercise 4 of homework 5

--- week5/Calc.hs
+++ week5/Calc.hs
@@ -5,15 +5,13 @@ module Calc where
 import ExprT
 import Parser
 
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7   = Mod7 Integer deriving (Eq, Show)
+
 class Expr a where
   lit :: Integer -> a
   add :: a -> a -> a
   mul :: a -> a -> a
-
-instance Expr ExprT where
-  lit = Lit
-  add = Add
-  mul = Mul
 
 eval :: ExprT -> Integer
 eval (Lit x)           = x
@@ -22,3 +20,43 @@ eval (Mul expr1 expr2) = (eval expr1) * (eval expr2)
 
 evalStr :: String -> Maybe Integer
 evalStr = fmap eval . parseExp Lit Add Mul
+
+instance Expr ExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
+
+instance Expr Integer where
+  lit     = id
+  add x y = x + y
+  mul x y = x * y
+
+instance Expr Bool where
+  lit x   = x > 0
+  add x y = x || y
+  mul x y = x && y
+
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax x) (MinMax y) = MinMax $ max x y
+  mul (MinMax x) (MinMax y) = MinMax $ min x y
+
+instance Expr Mod7 where
+  lit                   = Mod7 . (`mod` 7)
+  add (Mod7 x) (Mod7 y) = Mod7 $ (x + y) `mod` 7
+  mul (Mod7 x) (Mod7 y) = Mod7 $ (x * y) `mod` 7
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger :: Maybe Integer
+testInteger = testExp
+
+testBool :: Maybe Bool
+testBool = testExp
+
+testMM :: Maybe MinMax
+testMM = testExp
+
+testSat :: Maybe Mod7
+testSat = testExp
