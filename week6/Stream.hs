e721d120c22a42e12252f891029dbd3b63eaadf8
Tom Wadeson <tom@tomwadeson.com>
2016-04-13 18:14:02 +0100

    Solve exercise 4 of homework 6

--- week6/Stream.hs
+++ week6/Stream.hs
@@ -10,3 +10,12 @@ instance Show a => Show (Stream a) where
 
 streamToList :: Stream a -> [a]
 streamToList (Cons x xs) = x : streamToList xs
+
+streamRepeat :: a -> Stream a
+streamRepeat x = Cons x (streamRepeat x)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons x xs) = Cons (f x) (streamMap f xs)
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f x = Cons x (streamFromSeed f (f x))
