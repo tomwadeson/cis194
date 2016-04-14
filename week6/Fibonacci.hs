d9b38acbfb1b6af47a8334fd8cb3de589478c61e
Tom Wadeson <tom@tomwadeson.com>
2016-04-14 06:29:32 +0100

    Merged Stream.hs into Fibonacci.hs

--- week6/Fibonacci.hs
+++ week6/Fibonacci.hs
@@ -15,3 +15,30 @@ fibs2 :: [Integer]
 fibs2 = fib 0 1
   where
     fib x y = x : fib y (x+y)
+
+data Stream a = Cons a (Stream a)
+  deriving Eq
+
+instance Show a => Show (Stream a) where
+  show = show . take 20 . streamToList
+
+streamToList :: Stream a -> [a]
+streamToList (Cons x xs) = x : streamToList xs
+
+streamRepeat :: a -> Stream a
+streamRepeat x = Cons x (streamRepeat x)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons x xs) = Cons (f x) (streamMap f xs)
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f x = Cons x (streamFromSeed f (f x))
+
+nats :: Stream Integer
+nats = streamFromSeed (+1) 0
+
+ruler :: Stream Integer
+ruler = undefined
+
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (Cons x xs) (Cons y ys) = Cons x (Cons y (interleaveStreams xs ys))
