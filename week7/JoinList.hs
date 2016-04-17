d956dc9b2311c529d344f058631fdc1c48c70d94
Tom Wadeson <tom@tomwadeson.com>
2016-04-17 21:50:30 +0100

    WIP on exercise 2 of homework 7

--- week7/JoinList.hs
+++ week7/JoinList.hs
@@ -1,6 +1,7 @@
 module JoinList where
 
 import Data.Monoid ((<>))
+import Sized
 
 data JoinList m a = Empty
                   | Single m a
@@ -14,3 +15,10 @@ tag (Append m _ _) = m
 
 (++++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
 (++++) x y = Append (tag x <> tag y) x y
+
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ _ Empty          = Nothing
+indexJ n x@(Single _ y) = if (n == (getSize . size . tag $ x)) then (Just y) else Nothing
+indexJ n x@(Append _ l r)
+  | n <  (getSize . size . tag $ x) = indexJ n l
+  | n >  (getSize . size . tag $ x) = indexJ n r
