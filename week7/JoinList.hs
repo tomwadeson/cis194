63afa066b64ed9a6a9c144114d08f62a8e7a0b7f
Tom Wadeson <tom@tomwadeson.com>
2016-04-18 06:38:03 +0100

    Implement get-by-index for JoinLists

--- week7/JoinList.hs
+++ week7/JoinList.hs
@@ -17,11 +17,17 @@ tag (Append m _ _) = m
 (++++) x y = Append (tag x <> tag y) x y
 
 indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
-indexJ _ Empty          = Nothing
-indexJ n x@(Single _ y) = if (n == (getSize . size . tag $ x)) then (Just y) else Nothing
-indexJ n x@(Append _ l r)
-  | n <  (getSize . size . tag $ x) = indexJ n l
-  | n >  (getSize . size . tag $ x) = indexJ n r
+indexJ _ Empty = Nothing
+indexJ index (Single _ x) 
+  | index == 0 = Just x
+  | otherwise  = Nothing
+indexJ index (Append s l1 l2) 
+  | index < 0 || index > size0 = Nothing
+  | index < size1 = indexJ index l1
+  | otherwise     = indexJ (index - size1) l2
+  where
+    size0 = getSize . size $ s
+    size1 = getSize . size . tag $ l1
 
 (!!?) :: [a] -> Int -> Maybe a
 []     !!? _         = Nothing
@@ -35,11 +41,11 @@ jToList (Single _ x)     = [x]
 jToList (Append _ l1 l2) = jToList l1 ++ jToList l2
 
 m =
-  Append (Size 3)
+  Append (Size 2)
          (Single (Size 1) "Hello")
          (Single (Size 1) "World")
 
 n =
-  Append (Size 4)
+  Append (Size 3)
          (Single (Size 1) "Greeting:")
          m
