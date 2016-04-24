90083964fd39ee703685e9a52520f01deae31406
Tom Wadeson <tom@tomwadeson.com>
2016-04-24 13:01:29 +0100

    Solve homework 8

--- week8/Party.hs
+++ week8/Party.hs
@@ -1,6 +1,7 @@
 module Party where
 
 import Employee
+import Data.Tree
 
 instance Monoid GuestList where
   mempty                      = GL [] 0
@@ -11,3 +12,32 @@ glCons e (GL xs x) = GL (e : xs) (empFun e + x)
 
 moreFun :: GuestList -> GuestList -> GuestList
 moreFun gl1 gl2 = if gl1 > gl2 then gl1 else gl2
+
+treeFold :: (a -> [b] -> b) -> Tree a -> b
+treeFold f (Node label forest) = f label (map (treeFold f) forest)
+
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel boss results = (withBoss, withoutBoss)
+  where
+    withBoss    = glCons boss (mconcat $ map snd results)
+    withoutBoss = mconcat $ map (\(gl1, gl2) -> moreFun gl1 gl2) results
+
+maxFun :: Tree Employee -> GuestList
+maxFun = (\(gl1, gl2) -> moreFun gl1 gl2) . treeFold nextLevel
+
+prettyPrint :: GuestList -> String
+prettyPrint (GL list fun) = "Fun: " ++ show fun ++ "\n"
+                         ++ "Guest list:\n"
+                         ++ (unlines . map empName $ list) 
+
+main :: IO ()
+main = do
+  contents <- readFile "company.txt"
+  let guestlist = maxFun . read $ contents
+  putStrLn . prettyPrint $ guestlist
+
+t =
+  Node 0
+    [ Node 1 []
+    , Node 2 []
+    , Node 3 [ Node 4 [], Node 5 [] ] ]
